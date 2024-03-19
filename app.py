fdacaaa132c2d25fd5e773e3c7ea102630369c7f
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2024-03-19 22:04:24 +0100

    balazs-matyassy: notes-04-crud-view-state

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def home():
-    return render_template('home.html')
+    order = None
+    summary = False
+
+    if request.method == 'POST':
+        order = {
+            'product': request.form['product'],
+            'quantity': int(request.form['quantity'])
+        }
+
+        summary = True
+
+    return render_template('home.html', order=order, summary=summary)
 
 
-@app.route('/method', methods=('GET', 'POST'))
-def method():
-    return render_template('method.html', method=request.method)
+@app.route('/view-state', methods=('GET', 'POST'))
+def view_state():
+    order = {
+        'product': 'Androids',
+        'quantity': 1
+    }
+    summary = False
+
+    if request.method == 'POST':
+        order['product'] = request.form['product']
+        order['quantity'] = int(request.form['quantity'])
+        summary = True
+
+    return render_template('view_state.html', order=order, summary=summary)
 
 
 if __name__ == '__main__':
