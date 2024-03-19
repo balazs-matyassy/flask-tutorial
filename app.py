d197d1d72120745ae465c425bb268de3fadae844
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2024-03-19 23:26:16 +0100

    balazs-matyassy: notes-06-auth-login

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, request, session, g, redirect, url_for
+
+
+def load_current_user():
+    if session.get('user_id') is None:
+        g.username = None
+    else:
+        g.username = load_users()[session.get('user_id')]
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'dev'
 
-@app.route('/')
+app.before_request(load_current_user)
+
+
+@app.route('/', methods=('GET', 'POST'))
 def home():
-    return render_template('home.html')
+    users = load_users()
+
+    if request.method == 'POST':
+        user_id = int(request.form['user'])
+        session['user_id'] = user_id
+        session['username'] = users[user_id]
+
+        return redirect(url_for('home'))
+
+    return render_template('home.html', users=users)
 
 
-@app.route('/method', methods=('GET', 'POST'))
-def method():
-    return render_template('method.html', method=request.method)
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect(url_for('home'))
+
+
+def load_users():
+    with open('users.txt', encoding='utf-8') as file:
+        return [line.strip() for line in file]
 
 
 if __name__ == '__main__':
